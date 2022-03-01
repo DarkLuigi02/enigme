184b36ab3f7f5c71559d917fd6fb1cad10e9cc27
eleve <eleve@core>
2022-03-01 17:43:45 +0100

    v2

--- src/enigme.java
+++ src/enigme.java
@@ -2,16 +2,10 @@ import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
 public class enigme {
-    public int getTours() {
-        return tours;
-    }
 
-    int tours =0;
-    public void inittour(){this.tours=0;}
-    public void incrementtour(){tours++;}
+    public static void increment(int valeur){valeur++;}
 
-    public void init(String[][] plateau){
-        inittour();
+    public static void init(String[][] plateau){
         for (int i=0; i< plateau.length;i++){
             for (int j=0; j<plateau.length;j++){
                 if (Math.random()<=Math.random()){
@@ -22,25 +16,43 @@ public class enigme {
             }
         }
     }
-    public void initCoordonnées(String[][] plateau){
-        tours= getTours();
+    public static void initCoordonnées(String[][] plateau){
+        int tours =1;
         int y = 0,x=0;
         boolean fin=false;
         init(plateau);
-        plateau[y][x]="*";
         while(!fin){
+            increment(tours);
+            sortirJeu(plateau);
             System.out.println("Veuillez Saisir x (entre 0 et "+ (plateau.length-1)+")");
             x=saisirXY(plateau);
             System.out.println("Veuillez Saisir y (entre 0 et "+ (plateau.length-1)+")");
             y=saisirXY(plateau);
-            plateau[y][x]="*";
-            if (tours==tours%10){
-                System.out.println("votre nombre de tours et actuellement de"+ this.tours);
+            générationSuivante(plateau,x,y);
+            if (tours%9==0){
+                System.out.println("votre nombre de tours et actuellement de "+ tours);
             fin=stop();
             }
         }
-        System.out.println("vous avez reussi l'enigme en "+ getTours()+" tours");
+        boolean bool=false;
+        for (int i=0; i< plateau.length;i++){
+            for (int j=0; j<plateau.length;j++){
+                if (plateau[i][j]=="*"){
+                    verifier(bool);
+                }
+            }
+        }
+        if(bool==false){
+            System.out.println("vous avez reussi l'enigme en "+ tours +" tours");
+        }else{
+            System.out.println("Dommage peut etre une prochaine fois");
+        };
     }
+
+    private static boolean verifier(boolean bool) {
+        return bool=true;
+    }
+
     public static void sortirJeu(String[][] plateau) {
         for (int i=0; i< plateau.length;i++){
             for (int j=0; j<plateau.length;j++){
@@ -49,12 +61,12 @@ public class enigme {
             System.out.println();
         }
     }
-    public Integer nombreVoisin(String[][] plateau, Integer x, Integer y){
+    public static Integer voisin(String[][] plateau, Integer x, Integer y){
         Integer vivant=0;
         if (x==0){
             if (y==0){
                 if(plateau[y][x+1].equals("*")){
-                    vivant++;
+                    ;
                 }else{}
                 if(plateau[y+1][x].equals("*")){
                     vivant++;
@@ -186,38 +198,20 @@ public class enigme {
         return vivant;
     }
 
-    public void transfer(String[][] plateau1, String[][] plateau2){
+    public static void transfer(String[][] plateau1, String[][] plateau2){
         for (int i=0; i< plateau2.length;i++){
             for (int j=0; j<plateau2.length;j++){
-                plateau2[i][j]=plateau1[i][j]=" ";
+                plateau2[i][j]=plateau1[i][j];
             }
         }
     }
-    public void générationSuivante(String[][] plateau){
-        Scanner input = new Scanner(System.in);
+    public static void générationSuivante(String[][] plateau, int x , int y){
         String temp[][]=new String[10][10];
-        int nbvoisin;
         transfer(temp,plateau);
-        for (int y=0; y< plateau.length;y++){
-            for (int x=0; x<plateau.length;x++){
-                nbvoisin = nombreVoisin(plateau,x,y);
-                if (temp[y][x] == "*") {
-                    if (2 == nbvoisin || nbvoisin == 3){
-                        plateau[y][x] = "*";
-                    } else {
-                        plateau[y][x] = " ";
-                    }
-                } else if (temp[y][x] == " "){
-                    if (nbvoisin == 3) {
-                        plateau[y][x] = "*";
-                    } else {
-                        plateau[y][x] = " ";
-                    }
-                }
-            }
-        }
+        voisin(plateau,x,y);
     }
-    public Integer saisirXY(String[][] damier) {
+
+    public static Integer saisirXY(String[][] damier) {
         Scanner input = new Scanner(System.in);
         Integer i;
         try {
@@ -234,7 +228,7 @@ public class enigme {
     }
 
     // fonction pour arreter l'initialisatoin manuelle
-    public Boolean stop(){
+    public static Boolean stop(){
         Scanner input = new Scanner(System.in);
         String fin;
         Boolean finInit;
@@ -252,19 +246,18 @@ public class enigme {
     }
     public static void main(String[] args) throws InterruptedException {
             String[][] monplateau = new String[10][10];
-            init(monplateau);
             afficheTitre();
-            sortirJeu(monplateau);
+            initCoordonnées(monplateau);
     }
 
     private static void afficheTitre() {
         // http://patorjk.com/software/taag/
 
         System.out.println(
-                " ____            *  ____                                \n"+
+                " ____           *  ____                                \n"+
                 " |       \\   |  | /        |\\      /|    ___          \n" +
-                " |--     |\\  |  | |  --    | \\    / |   / _ \\         \n" +
-                " |       | \\ |  | \\    \\  |  \\  /  |   |  __/        \n" +
-                " |___    |  \\|  |   ---    |   \\/   |   \\ ___|        \n" );
+                " |--     |\\  |  | |  __    | \\    / |   / _ \\         \n" +
+                " |       | \\ |  | |     |   |  \\  /  |   |  __/        \n" +
+                " |___    |  \\|  |  ----    |   \\/   |   \\ ___|        \n" );
     }
 }
